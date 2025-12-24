c17be8cb944e62d3c8667d4072cd52cb0896276a
farmanwebdev <saqibfarman633@gmail.com>
2025-12-24 12:37:21 +0500

    test const, let ,var

---
+++ ClassOne.js
@@ -0,0 +1,48 @@
+console.log("Hello World")
+
+// This is our JS Comment
+userName = "farman" 
+console.log(userName);
+let userEmail = "saqib@gmail.com"
+console.log(userEmail);
+const userPass = "12345"
+console.log(userPass);
+
+var Location = "Pakistan"
+console.log(Location);  
+var PhoneNumber = "03001234567"
+console.log(PhoneNumber);
+var PhoneNumber = "03007654321"
+console.log(PhoneNumber);
+
+
+/*
+What is Scope?
+
+Why we should use var!
+
+*/
+
+
+
+
+
+
+// const UserName = "Ali"
+// let UserEmail = "admin@gmail.com"
+// var UserPass = "50269"
+// Location = "Gilgit"
+
+// let PhoneNumber;
+
+// UserName = "Basit"
+
+// console.log(UserName) 
+
+// UserEmail = "newadmin@gmail.com"
+// UserPass  = "112233"
+// Location = "Diamer"
+
+// console.table([UserEmail, UserPass, Location])
+
+// console.log(PhoneNumber);
