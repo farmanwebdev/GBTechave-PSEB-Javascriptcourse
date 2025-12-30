12180a6a74bd6b09837af75b21d44eace771de82
Farman Saqib <107495486+farmanwebdev@users.noreply.github.com>
2025-12-30 16:50:25 +0500

    Add files via upload

---
+++ ClassTwo.js
@@ -0,0 +1,50 @@
+//Simple If Condition
+
+let temperature = 30;
+if (temperature > 25) {
+  console.log("It's a hot day");
+}
+else {
+  console.log("It's a cool day");
+}
+
+// let age = 20;
+
+// if (age >= 20) {
+//   console.log("You are an adult");
+// }
+
+
+
+//is Else
+
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("You are an adult");
+// } else {
+//   console.log("You are a minor");
+// }
+
+
+// let score = 75;
+
+// if (score >= 90) {
+//   console.log("Grade: A");
+// } else if (score >= 70) {
+//   console.log("Grade: B");
+// } else if (score >= 50) {
+//   console.log("Grade: C");
+// } else {
+//   console.log("Grade: Fail");
+// }
+
+
+// let age = 20;
+// let hasID = true;
+
+// if (age >= 18 && hasID) {
+//   console.log("Entry allowed");
+// } else {
+//   console.log("Entry denied");
+// }
